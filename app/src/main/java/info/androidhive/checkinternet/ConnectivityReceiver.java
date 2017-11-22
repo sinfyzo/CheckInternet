245f814199e884554f0c1c85136f429fdb943fa2
shailesh <shailesh@infyzo.in>
2017-11-22 14:53:36 +0530

    Initial commit

---
+++ app/src/main/java/info/androidhive/checkinternet/ConnectivityReceiver.java
@@ -0,0 +1,50 @@
+package info.androidhive.checkinternet;
+
+/**
+ * Created by Lincoln on 18/03/16.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class ConnectivityReceiver
+        extends BroadcastReceiver {
+
+    public static ConnectivityReceiverListener connectivityReceiverListener;
+
+    public ConnectivityReceiver() {
+        super();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+        Log.e("onReceive","onReceive..."+isConnected);
+        if (connectivityReceiverListener != null) {
+            connectivityReceiverListener.onNetworkConnectionChanged(isConnected);
+        }
+    }
+
+    public static boolean isConnected() {
+        ConnectivityManager
+                cm = (ConnectivityManager) MyApplication.getInstance().getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+    }
+
+
+    public interface ConnectivityReceiverListener {
+        void onNetworkConnectionChanged(boolean isConnected);
+    }
+
+}
